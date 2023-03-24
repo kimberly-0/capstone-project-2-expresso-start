614a8d4586d947420bda7789ccec2b5b7f315633
Kimberly Dijkmans <kim.14420@outlook.com>
2023-03-24 21:44:49 +0100

    imported and mounted routers

---
+++ api/api.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = require('../server');
+
+const apiRouter = express.Router();
+module.exports = apiRouter;
+
+const employeesRouter = require('./employees');
+apiRouter.use('/employees', employeesRouter);
+
+const timesheetsRouter = require('./timesheets');
+employeesRouter.use('/:employeeId/timesheets', timesheetsRouter);
+
+const menusRouter = require('./menus');
+apiRouter.use('/menus', menusRouter);
+
+const menuitemsRouter = require('./menuitems');
+menusRouter.use('/:menuId/menuitems', menuitemsRouter);

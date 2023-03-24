95988050d4fc966f7489bfba411f90e4666c2eb8
Kimberly Dijkmans <kim.14420@outlook.com>
2023-03-24 22:21:21 +0100

    imported and mounted cors to all routes

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const errorhandler = require('errorhandler');
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(bodyParser.json());
+app.use(cors());
 app.use(errorhandler());
 
 const apiRouter = require('./api/api');
